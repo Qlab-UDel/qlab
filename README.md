01fc195730825e62abed1079973426570f700651
Manager <jasinskagroup@wifi-roaming-128-4-140-173.host.udel.edu>
2018-04-08 00:31:15 -0400

    rt_slope for 6_runs and nov_pilot handle cases when the subject misses the first target completely (does not respond during the following stimulus presentation) and write output files for individual results. Cleaning and rt_slope scripts for 6_runs handle the case when ltarget is F (no longer reads this value as false). 4_runs rt_slope script now separates files into the old and new formats. all accuracy scripts now create output files with individual results, and the 4_run vversion now standardizes all .corr columns as key_resp.corr (including for tsl files, which it previously did not).

---
+++ README.md
@@ -0,0 +1,5 @@
+# psychopy_sl_beh
+Nov pilot, 4 runs, 6 runs, SIT, PTT
+This folder contains in analysis scripts for the above experiments
+# apr8_psychopy_sl_beh_master
+# apr8_psychopy_sl_beh_master
---
+++ accuracy/nov_pilot/scripts/nov_pilot_accuracy_cleaning.R
@@ -0,0 +1,147 @@
+#  NOV_PILOT ACCURACY FILE CLEANING
+#  Violet Kozloff
+#  March 11, 2018
+#  This script cleans the auditory and visual files from the November pilot for accuracy analysis
+#  NOTE: Does not include participant 9, who did not respond in several types
+#  NOTE: Original psychopy files have been renamed to have a letter as a leading character. 
+#     To change names of files in terminal:
+#     for file in *.csv; do mv "$file" "m_$file"; done;
+#  NOTE: m_002_auditory_3.csv has been modified to include participant ID during the test session
+#  ****************************************************************************
+
+
+# Prepare files ------------------------------------------------------------
+
+# Move to folder from GitHub (Mac when folder is on desktop)
+setwd("/Users/jasinskagroup/Desktop/QLAB/psychopy_sl_beh-master/accuracy/nov_pilot/scripts/")
+
+# Remove objects in environment
+rm(list=ls())
+
+# Set up auditory input file path
+auditory_input_path <- ("../original_data/auditory/")
+auditory_files<- list.files(path=auditory_input_path, pattern="*.csv")
+
+# Separate tsl and ssl file paths
+tsl_files <- list.files(path=auditory_input_path, pattern="*6.csv")
+ssl_files <- list.files(path=auditory_input_path, pattern="*3.csv")
+
+# Set up audiotry ouput file path
+auditory_output_path <- "../cleaned_data/auditory/"
+
+# Set up visual file path
+visual_input_path <- ("../original_data/visual/")
+visual_files<- list.files(path=visual_input_path, pattern="*.csv")
+
+# Separate vsl and lsl file paths
+vsl_files <- list.files(path=visual_input_path, pattern="*6.csv")
+lsl_files <- list.files(path=visual_input_path, pattern="*3.csv")
+
+# Set up visual output file path
+visual_output_path <- "../cleaned_data/visual/"
+
+
+# Clean auditory files----------------------------------------------
+
+# tsl: create a new file containing only the relevant columns in the output folder
+tsl_cleaning <- function(file) {
+  current_file <- read.csv(file)
+  # Select relevant columns
+  value <- c("key_resp_6.corr", "Run","PartID","expName")
+  newdata <- current_file[value]
+  # Standardize "key_resp.corr" column across runs
+  names(newdata)[names(newdata) == 'key_resp_6.corr'] <- 'key_resp.corr'
+  # Remove any lines prior to test phase
+  newdata <- newdata[ which(!is.na(newdata$key_resp.corr)), ]
+  # Add domain
+  newdata$modality <- rep("non-linguistic",nrow(newdata))
+  # Add task
+  newdata$task <- rep("TSL",nrow(newdata))
+  #Write file
+  this_path<-file.path(auditory_output_path, basename(file))
+  write.csv(newdata, file=(this_path))
+}
+
+# Apply function to all tsl files
+for (file in tsl_files)
+{
+  tsl_cleaning(paste0(auditory_input_path,file))
+}
+
+# ssl: create a new file containing only the relevant columns in the output folder
+ssl_cleaning <- function(file) {
+  current_file <- read.csv(file)
+  # Select relevant columns
+  value <- c("question_key_resp.corr","Run","PartID","expName")
+  newdata <- current_file[value]
+  # Standardize "key_resp.corr" column across runs
+  names(newdata)[names(newdata) == 'question_key_resp.corr'] <- 'key_resp.corr'
+  # Remove any lines prior to test phase
+  newdata <- newdata[ which(!is.na(newdata$key_resp.corr)), ]
+  # Add domain
+  newdata$modality <- rep("linguistic",nrow(newdata))
+  # Add task
+  newdata$task <- rep("SSL",nrow(newdata))
+  # Write file
+  this_path<-file.path(auditory_output_path, basename(file))
+  write.csv(newdata, file=(this_path))
+}
+
+# Apply function to all ssl files
+for (file in ssl_files)
+{
+  ssl_cleaning(paste0(auditory_input_path,file))
+}
+
+
+# Clean visual files ----------------------------------------------------------
+
+# lsl: create a new file containing only the relevant columns in the output folder
+lsl_cleaning <- function(file) {
+  current_file <- read.csv(file)
+  # Select relevant columns
+  value <- c("question_key_resp.corr","Run","PartID","expName")
+  newdata <- current_file[value]
+  # Standardize "key_resp.corr" column across runs
+  names(newdata)[names(newdata) == 'question_key_resp.corr'] <- 'key_resp.corr'
+  # Remove any lines prior to test phase
+  newdata <- newdata[ which(!is.na(newdata$key_resp.corr)), ]
+  # Add domain
+  newdata$modality <- rep("linguistic",nrow(newdata))
+  # Add task
+  newdata$task <- rep("LSL",nrow(newdata))
+  # Write file
+  this_path<-file.path(visual_output_path, basename(file))
+  write.csv(newdata, file=(this_path))
+}
+
+# Apply function to all lsl files
+for (file in lsl_files)
+{
+  lsl_cleaning(paste0(visual_input_path,file))
+}
+
+# vsl: create a new file containing only the relevant columns in the output folder
+vsl_cleaning <- function(file) {
+  current_file <- read.csv(file)
+  # Select relevant columns
+  value <- c("question_key_resp.corr","Run","PartID","expName")
+  newdata <- current_file[value]
+  # Standardize "key_resp.corr" column across runs
+  names(newdata)[names(newdata) == 'question_key_resp.corr'] <- 'key_resp.corr'
+  # Remove any lines prior to test phase
+  newdata <- newdata[ which(!is.na(newdata$key_resp.corr)), ]
+  # Add domain
+  newdata$modality <- rep("non-linguistic",nrow(newdata))
+  # Add task
+  newdata$task <- rep("VSL",nrow(newdata))
+  # Write file
+  this_path<-file.path(visual_output_path, basename(file))
+  write.csv(newdata, file=(this_path))
+}
+
+# Apply function to all vsl files
+for (file in vsl_files)
+{
+  vsl_cleaning(paste0(visual_input_path,file))
+}
