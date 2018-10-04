1470903d96f00bbd21f11311e3017ceda27246ec
juliagoolia28 <41306746+juliagoolia28@users.noreply.github.com>
2018-10-04 10:57:35 -0400

    Create directory.m
    Establishes various directories to be called up for subsequent analyses.

---
+++ analysis/eeg-analysis/directory.m
@@ -0,0 +1,10 @@
+% script binding_paths
+% Sets paths for binding experiments
+% Julie Schneider
+% August 24, 2018
+
+anadir = 'Y:\projects\blast\data\eeg\Analysis\';
+rawdir = 'Y:\projects\blast\data\eeg\Analysis\rawdata\';
+workdir = 'Y:\projects\blast\data\eeg\Analysis\wkdir\';
+txtdir = 'Y:\projects\blast\data\eeg\Analysis\txtdir\';
+erpdir = 'Y:\projects\blast\data\eeg\Analysis\erpset\';
